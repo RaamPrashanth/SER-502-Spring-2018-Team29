88f55614b9d8fd200d6d6ab358511ba55f73d034
PRATIK SURYAWANSHI <32184670+pratik8064@users.noreply.github.com>
2018-04-22 20:19:55 -0700

    listener files

---
+++ src/com/ez/compiler/EZBaseListener.java
@@ -0,0 +1,316 @@
+package com.ez.compiler;
+
+// Generated from ../data/EZ.g4 by ANTLR 4.7.1
+
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+/**
+ * This class provides an empty implementation of {@link EZListener},
+ * which can be extended to create a listener which only needs to handle a subset
+ * of the available methods.
+ */
+public class EZBaseListener implements EZListener {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterProgram(EZParser.ProgramContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitProgram(EZParser.ProgramContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterStatement_list(EZParser.Statement_listContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitStatement_list(EZParser.Statement_listContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterStatement(EZParser.StatementContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitStatement(EZParser.StatementContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterDecl_statement(EZParser.Decl_statementContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitDecl_statement(EZParser.Decl_statementContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterAssign_statement(EZParser.Assign_statementContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitAssign_statement(EZParser.Assign_statementContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterRead_statement(EZParser.Read_statementContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitRead_statement(EZParser.Read_statementContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterWrite_statement(EZParser.Write_statementContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitWrite_statement(EZParser.Write_statementContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterIf_statement(EZParser.If_statementContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitIf_statement(EZParser.If_statementContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterLoop_statement(EZParser.Loop_statementContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitLoop_statement(EZParser.Loop_statementContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterComment_statement(EZParser.Comment_statementContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitComment_statement(EZParser.Comment_statementContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterExpression(EZParser.ExpressionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitExpression(EZParser.ExpressionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterExp1(EZParser.Exp1Context ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitExp1(EZParser.Exp1Context ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterCond_expression(EZParser.Cond_expressionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitCond_expression(EZParser.Cond_expressionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterCond_operators(EZParser.Cond_operatorsContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitCond_operators(EZParser.Cond_operatorsContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterIdentifier(EZParser.IdentifierContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitIdentifier(EZParser.IdentifierContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterLetters(EZParser.LettersContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitLetters(EZParser.LettersContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterUnderscore(EZParser.UnderscoreContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitUnderscore(EZParser.UnderscoreContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterBool_val(EZParser.Bool_valContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitBool_val(EZParser.Bool_valContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterNumber(EZParser.NumberContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitNumber(EZParser.NumberContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterDigit(EZParser.DigitContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitDigit(EZParser.DigitContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterLowerChar(EZParser.LowerCharContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitLowerChar(EZParser.LowerCharContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterUpperChar(EZParser.UpperCharContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitUpperChar(EZParser.UpperCharContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterSpaceChar(EZParser.SpaceCharContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitSpaceChar(EZParser.SpaceCharContext ctx) { }
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitTerminal(TerminalNode node) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitErrorNode(ErrorNode node) { }
+}
---
+++ src/com/ez/compiler/EZListener.java
@@ -0,0 +1,241 @@
+package com.ez.compiler;
+
+// Generated from ../data/EZ.g4 by ANTLR 4.7.1
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+
+/**
+ * This interface defines a complete listener for a parse tree produced by
+ * {@link EZParser}.
+ */
+public interface EZListener extends ParseTreeListener {
+	/**
+	 * Enter a parse tree produced by {@link EZParser#program}.
+	 * @param ctx the parse tree
+	 */
+	void enterProgram(EZParser.ProgramContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link EZParser#program}.
+	 * @param ctx the parse tree
+	 */
+	void exitProgram(EZParser.ProgramContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link EZParser#statement_list}.
+	 * @param ctx the parse tree
+	 */
+	void enterStatement_list(EZParser.Statement_listContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link EZParser#statement_list}.
+	 * @param ctx the parse tree
+	 */
+	void exitStatement_list(EZParser.Statement_listContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link EZParser#statement}.
+	 * @param ctx the parse tree
+	 */
+	void enterStatement(EZParser.StatementContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link EZParser#statement}.
+	 * @param ctx the parse tree
+	 */
+	void exitStatement(EZParser.StatementContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link EZParser#decl_statement}.
+	 * @param ctx the parse tree
+	 */
+	void enterDecl_statement(EZParser.Decl_statementContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link EZParser#decl_statement}.
+	 * @param ctx the parse tree
+	 */
+	void exitDecl_statement(EZParser.Decl_statementContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link EZParser#assign_statement}.
+	 * @param ctx the parse tree
+	 */
+	void enterAssign_statement(EZParser.Assign_statementContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link EZParser#assign_statement}.
+	 * @param ctx the parse tree
+	 */
+	void exitAssign_statement(EZParser.Assign_statementContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link EZParser#read_statement}.
+	 * @param ctx the parse tree
+	 */
+	void enterRead_statement(EZParser.Read_statementContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link EZParser#read_statement}.
+	 * @param ctx the parse tree
+	 */
+	void exitRead_statement(EZParser.Read_statementContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link EZParser#write_statement}.
+	 * @param ctx the parse tree
+	 */
+	void enterWrite_statement(EZParser.Write_statementContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link EZParser#write_statement}.
+	 * @param ctx the parse tree
+	 */
+	void exitWrite_statement(EZParser.Write_statementContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link EZParser#if_statement}.
+	 * @param ctx the parse tree
+	 */
+	void enterIf_statement(EZParser.If_statementContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link EZParser#if_statement}.
+	 * @param ctx the parse tree
+	 */
+	void exitIf_statement(EZParser.If_statementContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link EZParser#loop_statement}.
+	 * @param ctx the parse tree
+	 */
+	void enterLoop_statement(EZParser.Loop_statementContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link EZParser#loop_statement}.
+	 * @param ctx the parse tree
+	 */
+	void exitLoop_statement(EZParser.Loop_statementContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link EZParser#comment_statement}.
+	 * @param ctx the parse tree
+	 */
+	void enterComment_statement(EZParser.Comment_statementContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link EZParser#comment_statement}.
+	 * @param ctx the parse tree
+	 */
+	void exitComment_statement(EZParser.Comment_statementContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link EZParser#expression}.
+	 * @param ctx the parse tree
+	 */
+	void enterExpression(EZParser.ExpressionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link EZParser#expression}.
+	 * @param ctx the parse tree
+	 */
+	void exitExpression(EZParser.ExpressionContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link EZParser#exp1}.
+	 * @param ctx the parse tree
+	 */
+	void enterExp1(EZParser.Exp1Context ctx);
+	/**
+	 * Exit a parse tree produced by {@link EZParser#exp1}.
+	 * @param ctx the parse tree
+	 */
+	void exitExp1(EZParser.Exp1Context ctx);
+	/**
+	 * Enter a parse tree produced by {@link EZParser#cond_expression}.
+	 * @param ctx the parse tree
+	 */
+	void enterCond_expression(EZParser.Cond_expressionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link EZParser#cond_expression}.
+	 * @param ctx the parse tree
+	 */
+	void exitCond_expression(EZParser.Cond_expressionContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link EZParser#cond_operators}.
+	 * @param ctx the parse tree
+	 */
+	void enterCond_operators(EZParser.Cond_operatorsContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link EZParser#cond_operators}.
+	 * @param ctx the parse tree
+	 */
+	void exitCond_operators(EZParser.Cond_operatorsContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link EZParser#identifier}.
+	 * @param ctx the parse tree
+	 */
+	void enterIdentifier(EZParser.IdentifierContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link EZParser#identifier}.
+	 * @param ctx the parse tree
+	 */
+	void exitIdentifier(EZParser.IdentifierContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link EZParser#letters}.
+	 * @param ctx the parse tree
+	 */
+	void enterLetters(EZParser.LettersContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link EZParser#letters}.
+	 * @param ctx the parse tree
+	 */
+	void exitLetters(EZParser.LettersContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link EZParser#underscore}.
+	 * @param ctx the parse tree
+	 */
+	void enterUnderscore(EZParser.UnderscoreContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link EZParser#underscore}.
+	 * @param ctx the parse tree
+	 */
+	void exitUnderscore(EZParser.UnderscoreContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link EZParser#bool_val}.
+	 * @param ctx the parse tree
+	 */
+	void enterBool_val(EZParser.Bool_valContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link EZParser#bool_val}.
+	 * @param ctx the parse tree
+	 */
+	void exitBool_val(EZParser.Bool_valContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link EZParser#number}.
+	 * @param ctx the parse tree
+	 */
+	void enterNumber(EZParser.NumberContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link EZParser#number}.
+	 * @param ctx the parse tree
+	 */
+	void exitNumber(EZParser.NumberContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link EZParser#digit}.
+	 * @param ctx the parse tree
+	 */
+	void enterDigit(EZParser.DigitContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link EZParser#digit}.
+	 * @param ctx the parse tree
+	 */
+	void exitDigit(EZParser.DigitContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link EZParser#lowerChar}.
+	 * @param ctx the parse tree
+	 */
+	void enterLowerChar(EZParser.LowerCharContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link EZParser#lowerChar}.
+	 * @param ctx the parse tree
+	 */
+	void exitLowerChar(EZParser.LowerCharContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link EZParser#upperChar}.
+	 * @param ctx the parse tree
+	 */
+	void enterUpperChar(EZParser.UpperCharContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link EZParser#upperChar}.
+	 * @param ctx the parse tree
+	 */
+	void exitUpperChar(EZParser.UpperCharContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link EZParser#spaceChar}.
+	 * @param ctx the parse tree
+	 */
+	void enterSpaceChar(EZParser.SpaceCharContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link EZParser#spaceChar}.
+	 * @param ctx the parse tree
+	 */
+	void exitSpaceChar(EZParser.SpaceCharContext ctx);
+}
