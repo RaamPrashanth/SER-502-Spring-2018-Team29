c8a759ea247dd3b1255d81ef01cefbd17c247d0b
RaamPrashanth <rnamakka@asu.edu>
2018-04-27 01:23:27 -0700

    Constants file for handling intermediate and runtime

---
+++ src/com/ez/common/EZConstants.java
@@ -0,0 +1,33 @@
+package com.ez.common;
+
+public class EZConstants {
+	public static final String DECLARE = "DECL ";
+	public static final String ASSIGN = "MOV ";
+	public static final String ADD = "ADD ";
+	public static final String SUB = "SUB ";
+	public static final String MUL = "MUL ";
+	public static final String DIV = "DIV ";
+	public static final String REM = "REM ";
+	public static final String SPACE = " ";
+	public static final String PUSH = "PUSH ";
+	public static final String LOAD = "LOAD ";
+	public static final String READ = "READ ";
+	public static final String WRITE = "WRITE ";
+	public static final String IF = "IF ";
+	public static final String END_IF = "END IF ";
+	public static final String ELSE = "ELSE ";
+	public static final String LOOP = "LOOP ";
+	public static final String END_LOOP = "END LOOP ";
+	public static final String EQUAL = "EQUAL ";
+	public static final String LESS_THAN = "LESS_THAN ";
+	public static final String GREATER_THAN = "GREATER_THAN ";
+	public static final String LESS_EQUAL = "LESS_EQUAL ";
+	public static final String GREAT_EQUAL = "GREAT_EQUAL ";
+	public static final String NOT_EQUAL = "NOT_EQUAL ";
+	public static final String BOOL = "BOOL ";
+	public static final String COND_END = "COND_END ";
+	public static final String FUNC_CALL = "FUNC CALL ";
+	public static final String FUNC_DECL = "FUNC DECL ";
+	public static final String FUNC_END = "FUNC END ";
+	public static final String FUNC_CALL_WPARAM = "FUNC CALL WPARAM "; 
+}
